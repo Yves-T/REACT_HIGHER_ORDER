20e3715c3bb1b84aa16ccb55f36a29bb7828c9cf
Yves Talboom <yves1@MacBook-Pro-15inch.local>
2016-09-03 17:28:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React higher order component
+
+Demonstration using a higher order component to redirect user if he is not authenticated.
---
+++ src/components/resources.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div className="">
+            Super special recipe secret
+        </div>
+    );
+};
